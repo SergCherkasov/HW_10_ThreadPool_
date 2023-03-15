a2877f8487c4c2fc5bb0e5d2b902fe57122a3ba0
SergCherkasov <serg.cherkasov@gmail.com>
2023-03-15 11:01:17 +0200

    HW_10

---
+++ HW_10_ThreadPool/Program.cs
@@ -0,0 +1,17 @@
+﻿ThreadPool.SetMinThreads(1, 1);
+ThreadPool.SetMaxThreads(3, 2);
+
+for (int i = 0; i < 1000; i++)
+{
+    ThreadPool.QueueUserWorkItem(BackgroundTask, i);
+}
+
+
+Console.ReadKey();
+
+void BackgroundTask(object obj)
+{
+    int i = (int)obj;
+    Console.WriteLine($"Iter:{i}\nThreadPool?: {Thread.CurrentThread.IsThreadPoolThread}\nThreadID:{Thread.CurrentThread.ManagedThreadId}\nThreadsCount:{ThreadPool.ThreadCount}\n\n\n");
+    Thread.Sleep(10000);
+}
